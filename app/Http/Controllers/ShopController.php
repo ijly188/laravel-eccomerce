20d277b06ec66fa839a243dec637e2fb41eb8c80
terryyao <terry.yao.global@gmail.com>
2020-04-21 20:44:53 +0800

    part1, reset the gyp version, fix the npm problem, build products seeds data, rename main page to landing-page with scss, blade, route, create the LandingPageController, ShopController, Product Model

---
+++ app/Http/Controllers/ShopController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Product;
+
+class ShopController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Product::inRandomOrder()->take(12)->get();
+
+        return view('shop')->with('products', $products);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  string  $slug
+     * @return \Illuminate\Http\Response
+     */
+    public function show($slug)
+    {
+        $product = Product::where('slug', $slug)->firstOrFail();
+        $mightAlsoLike = Product::where('slug', '!=', $slug)->inRandomOrder()->take(4)->get();
+
+        return view('product')->with([
+            'product' => $product,
+            'mightAlsoLike' => $mightAlsoLike,
+        ]);
+    }
+}
